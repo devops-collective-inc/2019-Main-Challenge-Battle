1fbf736ff524b287ba2e35f513c6f458b5eade2e
Paul DeArment <paul.e.dearment.jr@gmail.com>
2019-05-02 14:55:49 -0700

    add module

---
+++ BattleModule/Test-Module.ps1
@@ -0,0 +1,36 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.162
+	 Created on:   	5/2/2019 2:53 PM
+	 Created by:   	PaulDeArment
+	 Organization: 	
+	 Filename:     	Test-Module.ps1
+	===========================================================================
+	.DESCRIPTION
+	The Test-Module.ps1 script lets you test the functions and other features of
+	your module in your PowerShell Studio module project. It's part of your project,
+	but it is not included in your module.
+
+	In this test script, import the module (be careful to import the correct version)
+	and write commands that test the module features. You can include Pester
+	tests, too.
+
+	To run the script, click Run or Run in Console. Or, when working on any file
+	in the project, click Home\Run or Home\Run in Console, or in the Project pane, 
+	right-click the project name, and then click Run Project.
+#>
+
+
+#Explicitly import the module for testing
+Import-Module 'BattleModule'
+
+#Run each module function
+Write-HelloWorld
+
+#Sample Pester Test
+#Describe "Test BattleModule" {
+#	It "tests Write-HellowWorld" {
+#		Write-HelloWorld | Should BeExactly "Hello World"
+#	}	
+#}
